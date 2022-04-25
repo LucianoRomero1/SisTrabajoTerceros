7b4013a062bc48e23c87d239eb36bf1a71ee4aa4
Luciano Romero <Lucianooromero1@gmail.com>
2022-04-25 12:54:00 -0300

    Testear si funciona mañana el maxvalue

--- web/js/ajaxModals.js
+++ web/js/ajaxModals.js
@@ -289,9 +289,9 @@ function setCantidad(from, cantidadInicial, cantidadLimite, res){
 
 function checkRetrabajar(from){
     var cantidad  = document.getElementsByClassName("cantidad");
-    //var maxValue =  cantidad[1].attr("max"); //Aca me guardo el value del max para volverlo a poner
+    var maxValue  =  0; //Aca me guardo el value del max para volverlo a poner
     console.log(Math.floor(cantidad[1].max));
-    var checkBox = "";
+    var checkBox  = "";
     switch(from){
         case "envio":
             checkBox = document.getElementById("check_envio");
@@ -309,34 +309,37 @@ function checkRetrabajar(from){
     if(checkBox.checked == true){
         switch(from){
             case "envio":
+                maxValue = cantidad[1].attr("max");
                 cantidad[1].removeAttribute("max");
                 break;
             case "recepcion":
+                maxValue = cantidad[3].attr("max");
                 cantidad[3].removeAttribute("max");
                 break;
             case "reingreso":
+                maxValue = cantidad[5].attr("max");
                 cantidad[5].removeAttribute("max");
                 break;
             case "devolucion":
+                maxValue = cantidad[7].attr("max");
                 cantidad[7].removeAttribute("max");
                 break;
         }
     }else{
-        console.log("unchecked");
+        switch(from){
+            case "envio":
+                cantidad[1].setAttribute("max", maxValue);
+                break;
+            case "recepcion":
+                cantidad[3].setAttribute("max", maxValue);
+                break;
+            case "reingreso":
+                cantidad[5].setAttribute("max", maxValue);
+                break;
+            case "devolucion":
+                cantidad[7].setAttribute("max", maxValue);
+                break;
+        }
     }
-    // switch(from){
-    //     case "envio":
-    //         cantidadLimite[1].removeAttribute("max");
-    //         break;
-    //     case "recepcion":
-    //         cantidadLimite[3].removeAttribute("max");
-    //         break;
-    //     case "reingreso":
-    //         cantidadLimite[5].removeAttribute("max");
-    //         break;
-    //     case "devolucion":
-    //         cantidadLimite[7].removeAttribute("max");
-    //         break;
-    // }
 }
 
